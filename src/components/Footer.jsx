0f3a16b86336eca0c78318dcbad34cf4978414c7
Lionnel Lubangi Mohamed <47186105+lubangi@users.noreply.github.com>
2023-02-09 16:15:05 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return <div className='bg-[#24C8FE] mb-0'>
       <div className="h-12 max-w-6xl mx-auto flex items-center justify-center">
-          <p className='text-lg text-slate-100'>Copyright @ Martin Devs {(new Date()).getFullYear()}</p>
+          <p className='text-lg text-slate-100'>Copyright @ Lionnel Lubangi Devs {(new Date()).getFullYear()}</p>
       </div>
   </div>;
 };
